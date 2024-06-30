29ee10eb3634c770d62a4ca2493053ba99657864
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-30 20:28:06 +0200

    Add class Aquila.java

---
+++ src/org/lessons/java/abstactclass/Aquila.java
@@ -0,0 +1,46 @@
+package org.lessons.java.abstactclass;
+
+public class Aquila extends Animale {
+	// instance variables
+	private String habitat;
+	private int age;
+	
+	// constructor method
+	public Aquila(String name, String species) {
+		super(name);
+		setSpecies(species);
+		setFood("altri animali");
+	}
+	public Aquila(String name, String species, String habitat, int age) {
+		super(name);
+		setSpecies(species);
+		setFood("altri animali");
+		this.age = age;
+		this.habitat = habitat;
+	}
+
+	@Override
+	public void whatEat() {
+		System.out.println(getName() + " eat " + getFood());
+	}
+	
+	@Override
+	public void makeASound() {
+		System.out.println(getName() + " is screeching.");
+	}
+	
+	//Getters and Setters
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
