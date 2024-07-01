71d415631c9b74465bc903755385dd335c9b25e8
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-07-01 15:34:21 +0200

    Add class AnimalStaticActions.java

---
+++ src/org/lessons/java/abstactclass/AnimalStaticActions.java
@@ -0,0 +1,14 @@
+package org.lessons.java.abstactclass;
+
+public class AnimalStaticActions {
+	
+	// static method faiVolare
+	public static void faiVolare(Volante animale) {
+		animale.vola();
+	}
+	
+	// static method faiNuotare
+	public static void faiNuotare(Nuotante animale) {
+		animale.nuota();
+	}
+}
